57a6cef14fad6fd068665fe8d8f92ce2eb9d3ad6
constantyn-silvian <costidimian@gmail.com>
2025-10-17 09:10:58 +0300

    Added partially css

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -1,14 +1,14 @@
 export default function MovieCard({ movie }) {
     return (
-        <div className="border-2 border-gray-600 rounded-md m-4 p-2">
+        <div className="border-2 border-gray-600 rounded-md m-4 p-2 bg-slate-800 mx-auto ">
             <div>
-                <img src="#" ></img>
+                <img src={movie.url} ></img>
                 <button>♡</button>
             </div>
             <div>
                 <h3 >{movie.title}</h3>
                 <p>{movie.releaseDate}</p>
             </div>
-        </div>
+        </div>      
     )
 }
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -3,12 +3,14 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <nav>
-      <div>
-        <Link to="/">Movie App</Link>
-      </div>
-      <div className="flex flex-row justify-evenly">
-        <Link to="/">Home</Link>
-        <Link to="/Favorites">Favorites</Link>
+      <div className="flex flex-row justify-between p-4 mb-4 bg-black">
+        <div>
+          <Link className="text-xl font-medium" to="/">Movie App</Link>
+        </div>
+        <div className="flex flex-row justify-evenly min-w-50">
+          <Link to="/">Home</Link>
+          <Link to="/Favorites">Favorites</Link>
+        </div>
       </div>
     </nav>
   );
