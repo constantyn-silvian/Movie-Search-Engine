201ecfdcc225e66837cfc11243fe300d2e426a66
constantyn-silvian <costidimian@gmail.com>
2025-10-20 01:50:06 +0300

    working backend

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -1,15 +1,32 @@
-export default function MovieCard({ movie }) {
+import { use, useEffect, useState } from "react";
+
+export default function MovieCard({ movie, source }) {
+    const [liked, setLiked] = useState(false);
+
+    useEffect(() => {
+        toggleLike();
+    }, [])
+
+    const toggleLike = () => {
+        if (!localStorage.getItem(`${movie.id}`)) {
+            localStorage.setItem(`${movie.id}`, JSON.stringify(movie));
+            setLiked(true);
+        }
+        else {
+            localStorage.removeItem(`${movie.id}`);
+            setLiked(false);
+        }
+    }
     return (
-        <div className="border-2 border-gray-600 hover:border-gray-400 hover:border-3 rounded-md m-1 p-2 max-w-[350px] bg-slate-800 ">
-            <div>
-                <img className="object-fill w-full" src={movie.url} ></img>
-            </div>
+        <div className="flex flex-col  border-2 border-gray-600 hover:border-gray-400 hover:border-3 rounded-md m-1 p-1 bg-slate-800 ">
+            <img className="object- w-full min-h-[150px]" src={`http://image.tmdb.org/t/p/w300${movie.poster_path}`} ></img>
             <div className="flex flex-row justify-between mt-2 ">
-                <div className=" text-left text-[clamp(20px,1.2vw,50px)] font-gray-200">
+                <div className=" text-left text-[clamp(10px,1.2vw,40px)] text-gray-300 font-semibold">
                     <h3 >{movie.title}</h3>
-                    <p >{movie.releaseDate}</p>
+                    <p >{movie.release_date}</p>
                 </div>
-                <button>♡</button>
+                {(source == "home") && <button onClick={toggleLike}>{liked ? "❤︎" : "♡"}</button>}
+                {(source == "favorites") && <button >❤︎</button>}
             </div>
         </div>
     )
--- frontend/src/pages/Favorites.jsx
+++ frontend/src/pages/Favorites.jsx
@@ -1,9 +1,31 @@
-export default function Favorites(){
+import { useEffect, useState } from "react"
+import MovieCard from "../components/movieCard";
 
+export default function Favorites() {
+    const [favoriteMovies, setFavoriteMovies] = useState([]);
+
+    useEffect(() => {
+        setFavoriteMovies(Object.keys(localStorage));
+    }, []);
+
+    useEffect(() => {
+        setFavoriteMovies(Object.keys(localStorage));
+    }, [localStorage.length])
+
+    const handleContent = () => {
+        let maxWidth = "1fr";
+        if(!favoriteMovies || favoriteMovies.length === 0) return <p className="text-lg">You don't have any favorites for the moment</p>
+        if(favoriteMovies.length < 4) {maxWidth = "400px";}
+        return (
+            <div className={`grid grid-cols-[repeat(auto-fit,minmax(250px,${maxWidth}))] gap-1 p-2`} >
+                    {favoriteMovies.map((movie_key) => (<MovieCard movie={JSON.parse(localStorage.getItem(movie_key))} source={"favorites"} key={movie_key} />))}
+            </div>
+        )   
+    }
     return (
         <div>
             <h1 className="text-5xl text-red-600 font-black">Favorite Movies:</h1>
-            <p className="text-lg">You don't have any favorites for the moment</p>
+            {handleContent()}
         </div>
     )
 }
